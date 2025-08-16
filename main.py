254e635944e3ee8ae004f235cdccc1b560d112ea
wiktor <wiktor.kycia@gmail.com>
2025-08-16 16:25:51 +0200

    create bands list

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+bands = [
+	{'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+	{'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
+	{'id': 3, 'name': 'Slowdive', 'genre': 'Shoegaze'},
+	{'id': 4, 'name': 'Wu-Tang Clean', 'genre': 'Hip-Hop'},
+]
+
 @app.get('/')
 async def index() -> dict[str, str]: # if there would be a list, fastapi will return an internal server error
 	return {'content': 'hello world!'}
