eb4f36ee212a566df323f68605a2a668fd355acb
wiktor <wiktor.kycia@gmail.com>
2025-08-30 14:25:15 +0200

    added query parameters to the get_random_number function

--- excercises/app1.py
+++ excercises/app1.py
@@ -12,6 +12,11 @@ async def home() -> str:
 	return "hello world!"
 
 @app.get('/random')
-async def get_random_number() -> RandomNumberType:
-	"""Returns a random number between 1 and 10"""
-	return RandomNumberType(**{"number": random.randint(1, 10)})
+async def get_random_number(a: int = 1, b: int = 10) -> RandomNumberType:
+	"""
+	Returns a random number between a and b including both values
+
+	:param a: int, from
+	:param b: int, to
+	"""
+	return RandomNumberType(**{"number": random.randint(a, b)})
