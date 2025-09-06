f80269249a5c5c1e2b4ee8321269a8a2c9f88057
wiktor <wiktor.kycia@gmail.com>
2025-09-06 19:15:57 +0200

    fix imports and comment all the endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Path, Query
-from schemas import GenreURLChoices, BandBase, BandCreate, BandWithID
+from schemas import GenreURLChoices, BandBase, BandCreate, Band, Album
 from typing import Annotated
 
 
@@ -18,45 +18,45 @@ BANDS = [
 	{'id': 4, 'name': 'Wu-Tang Clean', 'genre': 'Hip-Hop'},
 ]
 
-@app.get('/bands')
-async def bands(
-		genre: GenreURLChoices | None = None,
-		has_albums: bool = False,
-		q: Annotated[str | None, Query(max_length=10)] = None # returns an error (422) if the string is longer than 10
-) -> list[BandWithID]: # if there would be a list, fastapi will return an internal server error
-	bands_list: list[BandWithID] = [BandWithID(**b) for b in BANDS]
-	if has_albums:
-		bands_list = list(filter(lambda band: len(band.albums), bands_list))
-	if genre:
-		bands_list = list(filter(lambda band: band.genre.value.lower() == genre.value, bands_list))
-	if q:
-		bands_list = [
-			b for b in bands_list if q.lower() in b.name.lower()
-		]
-	return bands_list
-
-# @app.get('/bands/{band_id}', status_code=206) # if the response is successful, it will return this code
-@app.get('/bands/{band_id}')
-async def band(band_id: Annotated[int, Path(title="The band ID")]) -> BandWithID:
-	band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
-	if band is None:
-		raise HTTPException(status_code=404, detail='Band not found')
-
-	return band
-
-@app.get('/bands/genre/{genre}')
-async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:
-	return [
-		b for b in BANDS if b['genre'].lower() == genre.value
-	]
-
-
-@app.post('/bands')
-async def create_band(band_data: BandCreate) -> BandWithID:
-	id = BANDS[-1]['id'] + 1
-	band = BandWithID(id=id, **band_data.model_dump())
-	BANDS.append(band.model_dump())
-	return band
+# @app.get('/bands')
+# async def bands(
+# 		genre: GenreURLChoices | None = None,
+# 		has_albums: bool = False,
+# 		q: Annotated[str | None, Query(max_length=10)] = None # returns an error (422) if the string is longer than 10
+# ) -> list[BandWithID]: # if there would be a list, fastapi will return an internal server error
+# 	bands_list: list[BandWithID] = [BandWithID(**b) for b in BANDS]
+# 	if has_albums:
+# 		bands_list = list(filter(lambda band: len(band.albums), bands_list))
+# 	if genre:
+# 		bands_list = list(filter(lambda band: band.genre.value.lower() == genre.value, bands_list))
+# 	if q:
+# 		bands_list = [
+# 			b for b in bands_list if q.lower() in b.name.lower()
+# 		]
+# 	return bands_list
+#
+# # @app.get('/bands/{band_id}', status_code=206) # if the response is successful, it will return this code
+# @app.get('/bands/{band_id}')
+# async def band(band_id: Annotated[int, Path(title="The band ID")]) -> BandWithID:
+# 	band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
+# 	if band is None:
+# 		raise HTTPException(status_code=404, detail='Band not found')
+#
+# 	return band
+#
+# @app.get('/bands/genre/{genre}')
+# async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:
+# 	return [
+# 		b for b in BANDS if b['genre'].lower() == genre.value
+# 	]
+#
+#
+# @app.post('/bands')
+# async def create_band(band_data: BandCreate) -> BandWithID:
+# 	id = BANDS[-1]['id'] + 1
+# 	band = BandWithID(id=id, **band_data.model_dump())
+# 	BANDS.append(band.model_dump())
+# 	return band
 
 
 
