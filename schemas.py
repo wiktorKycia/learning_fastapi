d404643a5c2901d3a7d6756ef7515c7d1605f1bc
wiktor <wiktor.kycia@gmail.com>
2025-09-06 19:14:34 +0200

    introduce SQLModel, redesign schemas

--- schemas.py
+++ schemas.py
@@ -1,6 +1,8 @@
 from datetime import date
 from enum import Enum
 from pydantic import BaseModel, field_validator, validator
+from sqlalchemy import table
+from sqlmodel import SQLModel, Field, Relationship
 
 class GenreURLChoices(Enum):
 	ROCK = 'rock'
@@ -14,19 +16,25 @@ class GenreChoices(Enum):
 	METAL = 'Metal'
 	HIP_HOP = 'Hip-Hop'
 
-class Album(BaseModel):
+class AlbumBase(SQLModel):
 	title: str
 	release_date: date
+	band_id: int = Field(foreign_key="band.id")
 
-class BandBase(BaseModel):
+class Album(AlbumBase, table=True):
+	id: int = Field(default=None, primary_key=True)
+	band: "Band" = Relationship(back_populates="albums")
+
+class BandBase(SQLModel):
 	name: str
 	genre: GenreChoices
-	albums: list[Album] = [] # default value
 
 class BandCreate(BandBase):
+	albums: list[AlbumBase] | None = None  # default value
 	@validator('genre', pre=True)
 	def title_case_genre(cls, value):
 		return value.title()
 
-class BandWithID(BandBase):
-	id: int
+class Band(BandBase, table=True):
+	id: int = Field(default=None, primary_key=True)
+	albums: list[Album] = Relationship(back_populates="band")
